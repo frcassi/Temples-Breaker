c3f06bc5c48afd44a6e0f696e8cd1a6c9fa567a0
Federico Cassissa <39331053+frcassi@users.noreply.github.com>
2021-07-16 15:29:35 +0200

    Added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    private float xBound = 21;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerMovement();
+    }
+
+    // Moves the player based on arrow key input
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+
+    // Prevent player from going out of boundaries horizontally
+    void ConstrainPlayerMovement()
+    {
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
+    }
+}
